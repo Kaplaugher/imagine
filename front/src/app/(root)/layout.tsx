309448ef300c6b662ec7d268fc92aa8445f5bfa2
kyle <kaplaugher94@gmail.com>
2024-02-27 14:25:23 -0500

    side nav

--- front/src/app/(root)/layout.tsx
+++ front/src/app/(root)/layout.tsx
@@ -1,17 +1,15 @@
 import Sidebar from "@/components/shared/Sidebar";
-import React from "react";
 
-function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
-      {/* sidebar */}
       <Sidebar />
-      {/* mobile nav */}
+
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
     </main>
   );
-}
+};
 
 export default Layout;
