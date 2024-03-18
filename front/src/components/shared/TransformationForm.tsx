fdcf25a0e6f1a747546a8096a895e36be486232f
kyle <kaplaugher94@gmail.com>
2024-03-18 16:15:18 -0400

    home page

--- front/src/components/shared/TransformationForm.tsx
+++ front/src/components/shared/TransformationForm.tsx
@@ -3,11 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { CustomField } from "./CustomField";
-import { useRouter } from "next/navigation";
 
 import {
   Select,
@@ -16,16 +11,34 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { aspectRatioOptions, transformationTypes } from "@/constants";
-import { useState, useTransition } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  aspectRatioOptions,
+  creditFee,
+  defaultValues,
+  transformationTypes,
+} from "@/constants";
+import { CustomField } from "./CustomField";
+import { useEffect, useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
-import { set } from "mongoose";
-import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
-import { Transform } from "stream";
 import TransformedImage from "./TransformedImage";
+import { updateCredits } from "@/lib/actions/user.actions";
 import { getCldImageUrl } from "next-cloudinary";
 import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -35,15 +48,14 @@ export const formSchema = z.object({
   publicId: z.string(),
 });
 
-export function TransformationForm({
-  data = null,
+export const TransformationForm = ({
   action,
+  data = null,
   userId,
   type,
   creditBalance,
   config = null,
-}: TransformationFormProps) {
-  const router = useRouter();
+}: TransformationFormProps) => {
   const transformationType = transformationTypes[type];
   const [image, setImage] = useState(data);
   const [newTransformation, setNewTransformation] =
@@ -52,6 +64,8 @@ export function TransformationForm({
   const [isTransforming, setIsTransforming] = useState(false);
   const [transformationConfig, setTransformationConfig] = useState(config);
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
   const initialValues =
     data && action === "Update"
       ? {
@@ -61,19 +75,15 @@ export function TransformationForm({
           prompt: data?.prompt,
           publicId: data?.publicId,
         }
-      : {
-          title: "",
-          aspectRatio: "",
-          color: "",
-          prompt: "",
-          publicId: "",
-        };
+      : defaultValues;
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialValues,
   });
 
+  // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
     setIsSubmitting(true);
 
@@ -145,13 +155,16 @@ export function TransformationForm({
     onChangeField: (value: string) => void
   ) => {
     const imageSize = aspectRatioOptions[value as AspectRatioKey];
+
     setImage((prevState: any) => ({
       ...prevState,
       aspectRatio: imageSize.aspectRatio,
       width: imageSize.width,
       height: imageSize.height,
     }));
+
     setNewTransformation(transformationType.config);
+
     return onChangeField(value);
   };
 
@@ -162,31 +175,42 @@ export function TransformationForm({
     onChangeField: (value: string) => void
   ) => {
     debounce(() => {
-      setTransformationConfig((prevState: any) => ({
+      setNewTransformation((prevState: any) => ({
         ...prevState,
         [type]: {
-          ...prevState[type],
+          ...prevState?.[type],
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
-    }, 1000);
+    }, 1000)();
+
     return onChangeField(value);
   };
 
   const onTransformHandler = async () => {
     setIsTransforming(true);
+
     setTransformationConfig(
       deepMergeObjects(newTransformation, transformationConfig)
     );
+
     setNewTransformation(null);
+
     startTransition(async () => {
-      await updateCredits(userId, creditBalance - 1);
+      await updateCredits(userId, creditFee);
     });
   };
+
+  useEffect(() => {
+    if (image && (type === "restore" || type === "removeBackground")) {
+      setNewTransformation(transformationType.config);
+    }
+  }, [image, transformationType.config, type]);
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField
           control={form.control}
           name="title"
@@ -194,6 +218,7 @@ export function TransformationForm({
           className="w-full"
           render={({ field }) => <Input {...field} className="input-field" />}
         />
+
         {type === "fill" && (
           <CustomField
             control={form.control}
@@ -205,9 +230,10 @@ export function TransformationForm({
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
-                  <SelectValue placeholder="Select Size" />
+                  <SelectValue placeholder="Select size" />
                 </SelectTrigger>
                 <SelectContent>
                   {Object.keys(aspectRatioOptions).map((key) => (
@@ -222,51 +248,55 @@ export function TransformationForm({
         )}
 
         {(type === "remove" || type === "recolor") && (
-          <CustomField
-            control={form.control}
-            name="prompt"
-            formLabel={
-              type === "remove" ? "Object to remove" : "Object to recolor"
-            }
-            className="w-full"
-            render={({ field }) => (
-              <Input
-                className="input-field"
-                value={field.value}
-                onChange={(e) =>
-                  onInputChangeHandler(
-                    "prompt",
-                    e.target.value,
-                    type,
-                    field.onChange
-                  )
-                }
+          <div className="prompt-field">
+            <CustomField
+              control={form.control}
+              name="prompt"
+              formLabel={
+                type === "remove" ? "Object to remove" : "Object to recolor"
+              }
+              className="w-full"
+              render={({ field }) => (
+                <Input
+                  value={field.value}
+                  className="input-field"
+                  onChange={(e) =>
+                    onInputChangeHandler(
+                      "prompt",
+                      e.target.value,
+                      type,
+                      field.onChange
+                    )
+                  }
+                />
+              )}
+            />
+
+            {type === "recolor" && (
+              <CustomField
+                control={form.control}
+                name="color"
+                formLabel="Replacement Color"
+                className="w-full"
+                render={({ field }) => (
+                  <Input
+                    value={field.value}
+                    className="input-field"
+                    onChange={(e) =>
+                      onInputChangeHandler(
+                        "color",
+                        e.target.value,
+                        "recolor",
+                        field.onChange
+                      )
+                    }
+                  />
+                )}
               />
             )}
-          />
+          </div>
         )}
-        {type === "recolor" && (
-          <CustomField
-            control={form.control}
-            name="color"
-            formLabel="Replacement Color"
-            className="w-full"
-            render={({ field }) => (
-              <Input
-                className="input-field"
-                value={field.value}
-                onChange={(e) =>
-                  onInputChangeHandler(
-                    "color",
-                    e.target.value,
-                    "recolor",
-                    field.onChange
-                  )
-                }
-              />
-            )}
-          />
-        )}
+
         <div className="media-uploader-field">
           <CustomField
             control={form.control}
@@ -282,6 +312,7 @@ export function TransformationForm({
               />
             )}
           />
+
           <TransformedImage
             image={image}
             type={type}
@@ -291,19 +322,19 @@ export function TransformationForm({
             transformationConfig={transformationConfig}
           />
         </div>
+
         <div className="flex flex-col gap-4">
           <Button
             type="button"
-            className=" submit-button capitalize"
+            className="submit-button capitalize"
             disabled={isTransforming || newTransformation === null}
             onClick={onTransformHandler}
           >
             {isTransforming ? "Transforming..." : "Apply Transformation"}
           </Button>
-
           <Button
             type="submit"
-            className=" submit-button capitalize"
+            className="submit-button capitalize"
             disabled={isSubmitting}
           >
             {isSubmitting ? "Submitting..." : "Save Image"}
@@ -312,4 +343,6 @@ export function TransformationForm({
       </form>
     </Form>
   );
-}
+};
+
+export default TransformationForm;
